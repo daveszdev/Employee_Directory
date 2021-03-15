050416e024ff603720eb81fa9885430c227f9110
David Mate <davesz.dev@gmail.com>
2021-03-15 18:39:06 +0100

    Added updates
    Repositioned modal window and updated script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const modalHTML = `
 <p class="address">${city}</p>
 <hr />
 <p>${phone}</p>
-<p class="address">${street}, ${state} ${postcode}</p>
+<p class="address">${street.number} ${street.name} ${state} ${postcode}</p>    
 <p>Birthday:
 ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
 </div>
